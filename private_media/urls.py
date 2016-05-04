fd723e6bf59af4a4cc86f51969ed55e2111fd973
nickaein-a <nickaein.a@gmail.com>
2016-05-04 12:37:09 +0430

    fixed deprecated patterns error
    django.conf.urls.patterns is deprecated

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 
-urlpatterns = patterns('private_media.views',
-    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
-)
+urlpatterns = [
+    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), serve_private_file)
+]
+
